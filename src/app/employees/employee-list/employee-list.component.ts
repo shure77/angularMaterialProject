0a71ddab42996968e5d243641674f5701e4b0eff
Thaler <Roman.Thaler@adesso.at>
2019-07-25 10:58:41 +0200

    add department name to the table, even if it comes from another database entity

--- src/app/employees/employee-list/employee-list.component.ts
+++ src/app/employees/employee-list/employee-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { EmployeeService } from 'src/app/shared/employee.service';
 import { MatTableDataSource, MatSort, MatPaginator } from '@angular/material';
+import { DepartmentService } from 'src/app/shared/department.service';
 
 @Component({
   selector: 'app-employee-list',
@@ -18,12 +19,12 @@ export class EmployeeListComponent implements OnInit {
 
   //displayedColumns is hooked to the templates mat-header-row tag (there to the *matHeaderRowDef directive)and defines the header column names
   //wenn die strings im array entfernt werden oder anders sortiert werden, dann kann die Spalte entfernt oder anders angeordnet werden
-  displayedColumns: string[] = ['fullName', 'email', 'mobile', 'city', 'actions'];
+  displayedColumns: string[] = ['fullName', 'email', 'mobile', 'city', 'departmentName', 'actions'];
 
   @ViewChild(MatSort, {static: true}) sort: MatSort;
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
 
-  constructor(private service: EmployeeService) { }
+  constructor(private service: EmployeeService, private departmentService: DepartmentService) { }
 
   //in ngOnInit I get the employee entries from firebase. getEmployees returns an observable, i have to subscribe to! 
   //inside the subscribe function I have to convert the data into an array
@@ -31,10 +32,13 @@ export class EmployeeListComponent implements OnInit {
     this.service.getEmployees().subscribe(
       list => {
         this.employeesArray = list.map(item => {
+          //here i initialize the departmentName property by calling the getDepartmentName of the departmentService
+          let departmentName = this.departmentService.getDepartmentName(item.payload.val()['department']);
           //here we return an object containing the employee details
           return {
             //we also can assign the Key for each employee!
             $key: item.key,
+            departmentName, // short for departmentName: departmentName
             //...item.payload.val() returns the object containing the details
             // ...for destructuring -> SPREAD-OPERATOR!
             ...item.payload.val()
--- src/app/shared/department.service.ts
+++ src/app/shared/department.service.ts
@@ -1,26 +1,35 @@
-import { Injectable } from '@angular/core';
-import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { Injectable } from "@angular/core";
+import { AngularFireDatabase, AngularFireList } from "angularfire2/database";
+
+//lodash is an npm package which is installed via cli command: npm i --s lodash
+import * as _ from "lodash";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class DepartmentService {
-
   //we get the departments collection from firebase into departmentList and convert it into an array and save it into the array property
   departementList: AngularFireList<any>;
   array = [];
 
-  constructor(private firebase: AngularFireDatabase) { 
-    this.departementList = this.firebase.list('departments');
-    this.departementList.snapshotChanges().subscribe(
-      list => {
-        this.array = list.map(item => {
-          return {
-            $key: item.key,
-            ...item.payload.val()
-          }
-        });
-      }
-    );
+  constructor(private firebase: AngularFireDatabase) {
+    this.departementList = this.firebase.list("departments");
+    this.departementList.snapshotChanges().subscribe(list => {
+      this.array = list.map(item => {
+        return {
+          $key: item.key,
+          ...item.payload.val()
+        };
+      });
+    });
+  }
+
+  getDepartmentName($key) {
+    if ($key == "0") {
+      return "";
+    } else {
+      //this function will return all departments which match $key
+      return _.find(this.array, (obj) => { return obj.$key == $key; })['name'];
+    }
   }
 }
